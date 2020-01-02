452bc42b430dd10d589a20cc057e3831c40fa61d
jksingh72 <jk.singh72@gmail.com>
2020-01-02 17:03:20 -0500

    first commit

---
+++ JS_Sandbox/app.js
@@ -0,0 +1,45 @@
+// // alert('hello world2');
+// // console.log([1, 2, 3, 4]);
+// // console.log({ a: 1, b: 2 });
+// // console.table({ a: 1, b: 2 })
+
+// // console.error('some error');
+
+// //data types
+// //var name = 'JKSINGH';
+
+// let age;
+// let name;
+
+// name = 'jksingh';
+// age = 30
+
+// console.log('name is :' + name);
+
+// console.log(typeof name);
+
+
+// console.log('age is :' + age);
+
+// //console.log('name is ');
+// //console.log(1000 + name);
+// //console.log(typeof name);
+
+// console.log('jksingh-jksingh');
+
+// if (age > 21)
+//     console.log(name + ' is adult');
+// console.log(`${name}   is adult`);
+// //else
+// console.log(name + ' is below 21');
+// console.log(`${name}   is underage`);
+
+
+
+let val;
+val = document;
+val = document.all;
+val = document.all[1];
+
+console.log(val);
+
